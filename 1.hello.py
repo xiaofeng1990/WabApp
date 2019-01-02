ae43d29087665c7867802b58c3d46b81be399c7a
xiaofeng1990 <2840824715@qq.com>
2019-01-02 22:55:57 +0800

    完成配置flask环境文档 完成一个hello world

---
+++ 1.hello.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+# 使用装饰器的路由解析URL
+@app.route('/')
+def index():
+	return '<h1> Hello 李锦霞!</h1>'
+	
+@app.route('/user/<name>')
+def user(name):
+	return '<h1> Hello, %s!</h1>'% name
+
+# host='0.0.0.0' 可以外网访问
+if __name__ == "__main__":
+	app.run(host='0.0.0.0',debug=True)
---
+++ README.txt
@@ -0,0 +1,8 @@
+目录结构
+	backup/		备份目录
+	conf/		配置文件
+	dist/		打包目录
+	www/		web目录，存放.py文件
+	  static/	存放静态文件
+	  templates	存放模板文件
+	LICENSE		代码LICENSE
